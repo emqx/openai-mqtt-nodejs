80a9bcf0e57120cfa4cf4240cbfbab98ad46b9bd
YuShifan <894402575bt@gmail.com>
2023-04-05 22:46:24 +0800

    Implemented user isolation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Add this line at the beginning of your script
+require('dotenv').config();
 const axios = require("axios");
 const mqtt = require("mqtt");
 
-// Replace with your OpenAI API key
-const OPENAI_API_KEY = "Your-OpenAI-API-Key";
+// Add your OpenAI API key to your environment variables in .env
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 let messages = []; // Store conversation history
 const maxMessageCount = 10;
 
@@ -28,50 +30,44 @@ const OPTIONS = {
 };
 
 const connectUrl = `mqtt://${host}:${port}`;
-const chatGPTReqTopic = "chatgpt/#";
-
+const chatGPTReqTopic = "chatgpt/demo";
 const client = mqtt.connect(connectUrl, OPTIONS);
+const chatGPTReqTopicPrefix = "chatgpt/request/";
 
 client.on("connect", () => {
   console.log(`${host}, Connected`);
   client.subscribe(chatGPTReqTopic, () => {
-    console.log(`${host}, Subscribed to topic '${chatGPTReqTopic}'`);
+    console.log(`${host}, Subscribed to topics with prefix 'chatgpt/'`);
   });
-});
-
-client.on("reconnect", () => {
-  console.log(`Reconnecting(${host})`);
-});
-
-client.on("error", (error) => {
-  console.log(`Cannot connect(${host}, `, error);
 });
 
 client.on("message", (topic, payload) => {
   console.log("Received Message:", topic, payload.toString());
-  messages.push({ role: "user", content: payload.toString() });
-  if (messages.length > maxMessageCount) {
-    messages.shift(); // Remove the oldest message
+  const userId = topic.replace(chatGPTReqTopicPrefix, "");
+  messages[userId] = messages[userId] || [];
+  messages[userId].push({ role: "user", content: payload.toString() });
+  if (messages[userId].length > maxMessageCount) {
+    messages[userId].shift(); // Remove the oldest message
   }
-  genText();
+  genText(userId);
 });
 
-const genText = async () => {
+const genText = async (userId) => {
   try {
     const { data } = await http.post("/completions", {
       model: "gpt-3.5-turbo",
-      messages: messages,
+      messages: messages[userId],
       temperature: 0.7,
     });
-
     if (data.choices && data.choices.length > 0) {
       const { content } = data.choices[0].message;
       console.log(content);
-      messages.push({ role: "assistant", content: content });
-      if (messages.length > maxMessageCount) {
-        messages.shift(); // Remove the oldest message
+      messages[userId].push({ role: "assistant", content: content });
+      if (messages[userId].length > maxMessageCount) {
+        messages[userId].shift(); // Remove the oldest message
       }
-      client.publish("chatgpt/demo", content, { qos: 0, retain: false }, (error) => {
+      const replyTopic = `chatgpt/response/${userId}`;
+      client.publish(replyTopic, content, { qos: 0, retain: false }, (error) => {
         if (error) {
           console.error(error);
         }
